bf0b3fbba0f64d1f8b3376e2f0c9c4447925987c
Chandler Anderson <chandler.anderson@slalom.com>
2019-12-04 11:40:30 -0500

    Refactor for Day 2 part 2 solution.

--- 02/02.js
+++ 02/02.js
@@ -1,107 +1,107 @@
-let input = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,2,19,6,23,1,23,5,27,1,9,27,31,1,31,10,35,2,35,9,39,1,5,39,43,2,43,9,47,1,5,47,51,2,51,13,55,1,55,10,59,1,59,10,63,2,9,63,67,1,67,5,71,2,13,71,75,1,75,10,79,1,79,6,83,2,13,83,87,1,87,6,91,1,6,91,95,1,10,95,99,2,99,6,103,1,103,5,107,2,6,107,111,1,10,111,115,1,115,5,119,2,6,119,123,1,123,5,127,2,127,6,131,1,131,5,135,1,2,135,139,1,139,13,0,99,2,0,14,0];
+const example1 = [1,0,0,0,99];
+const example2 = [2,3,0,3,99];
+const example3 = [2,4,4,5,99,0];
+const example4 = [1,1,1,4,99,5,6,0,99];
+const puzzleInput = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,1,6,19,2,19,6,23,1,23,5,27,1,9,27,31,1,31,10,35,2,35,9,39,1,5,39,43,2,43,9,47,1,5,47,51,2,51,13,55,1,55,10,59,1,59,10,63,2,9,63,67,1,67,5,71,2,13,71,75,1,75,10,79,1,79,6,83,2,13,83,87,1,87,6,91,1,6,91,95,1,10,95,99,2,99,6,103,1,103,5,107,2,6,107,111,1,10,111,115,1,115,5,119,2,6,119,123,1,123,5,127,2,127,6,131,1,131,5,135,1,2,135,139,1,139,13,0,99,2,0,14,0];
 
-let startPos = 0;
-let optCode = 0;
-let noun = 0;
-let verb = 0;
+runIntcode = (input, noun, verb) => {
+    let memory = [].concat(input);
 
-runIntcode = (arr) => {
-    for (let i in arr) {
-        let x = arr[i];
-        let pos = i - startPos;
-        console.log('index ' + i + ' with value ' + x + ' is position ' + pos);
-        switch (pos) {
+    if (noun && verb) {
+        memory[1] = noun;
+        memory[2] = verb;
+        console.log('noun: ' + noun + ' verb: ' + verb);
+    }
+    console.log('the starting array is ' + input);
+
+    let instructionStart = 0;
+    let optCode = 0;
+    let param1 = 0;
+    let param2 = 0;
+
+    memory.forEach((x, address) => {
+        let instructionPointer = address - instructionStart;
+        //console.log('index ' + address + ' with value ' + x + ' is position ' + instructionPointer);
+        switch (instructionPointer) {
             case 0:
                 optCode = x;
-                console.log('optCode is now ' + optCode);
                 break;
             case 1:
-                noun = arr[x];
-                console.log('noun is now ' + noun);
+                param1 = memory[x];
                 break;
             case 2:
-                verb = arr[x];
-                console.log('verb is now ' + verb);
+                param2 = memory[x];
                 break;
             case 3:
-                let calcResult = doCalc(optCode, noun, verb);
-                arr[x] = calcResult;
-                console.log('calc result is ' + calcResult);
+                let calcResult = doCalc(optCode, param1, param2);
 
-                if (calcResult === 19690720) {
-                    console.log('THIS IS THE ANSWER')
+                if (calcResult < 0) {
+                    // Less than 0 means that optcode 99 or unknown was provided.
+                    console.log('PROGRAM ENDED: address ' + address + ' optcode ' + optCode + ' param1 ' + param1 + ' param2 ' + param2);
+                    return memory;
                 }
 
-                console.log('array is now ' + arr);
+                memory[x] = calcResult;
+
+                instructionStart += 4;
+                optCode = 0;
+                param1 = 0;
+                param2 = 0;
+
                 break;
         }
+    });
 
-        //if (optCode === 99) resetMemory(1);
-        if (pos === 3)  resetMemory(4);
-
-        console.log('----');
-    }
-
-    return arr;
+    return memory;
 };
 
-doCalc = (optCode, val1, val2) => {
+doCalc = (optCode, x, y) => {
     switch (optCode) {
         case 1:
-            return val1 + val2;
+            return x + y;
         case 2:
-            return val1 * val2;
-        default:
-            return;
-    }
-};
-
-doCalc2 = (arr) => {
-    let optCode = arr[0];
-    switch (optCode) {
-        case 1:
-            return val1 + val2;
-        case 2:
-            return val1 * val2;
+            return x * y;
         case 99:
+            console.log('Optcode 99');
             return -1;
         default:
-            return;
+            console.log('Optcode ' + optCode);
+            return -2;
     }
 };
 
-resetMemory = (x) => {
-    startPos += x;
-    console.log('start position is now ' + startPos);
+part1 = (input, noun, verb) => {
+    console.log('-- PART ONE --');
+    let answer = runIntcode(input, noun, verb);
 
-    optCode = 0;
-    noun = 0;
-    verb = 0;
-};
-
-demo = arr => {
-    let answer = runIntcode(arr);
     console.log('the final array is ' + answer);
-    return answer;
+    console.log('the first item is ' + answer[0]);
+    return answer[0];
 };
 
-part1 = arr => {
-    let errorState = arr => {
-        arr[1] = 12;
-        arr[2] = 2;
-        return arr;
-    };
+part2 = (arr) => {
+    console.log('-- PART TWO --');
+    for (let noun = 0; noun <= 99; noun++) {
+        for (let verb = 0; verb < 99; verb++) {
+            let res = runIntcode(arr, noun, verb);
+            if (res[0] === 19690720) {
+                let answer = (100 * noun) + verb;
+                console.log('THIS IS THE ANSWER: ' + answer);
+                return answer;
+            }
 
-    let answer = runIntcode(errorState(arr));
-    console.log(answer[0]);
-    return answer;
+            console.log('the final array is ' + res);
+            console.log('the first item is ' + res[0]);
+            console.log('----');
+        }
+    }
+
+    console.log('NO ANSWER FOUND');
 };
 
-part2 = arr => {
-    let answer = runIntcode(arr);
-    console.log(answer[0]);
-    return answer;
-};
-
-//demo([2,4,4,5,99,0]);
-part1(input);
+part1(example1);
+part1(example2);
+part1(example3);
+part1(example4);
+part1([].concat(puzzleInput), 12, 2);
+part2([].concat(puzzleInput));
